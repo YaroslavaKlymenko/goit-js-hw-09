774fcbca9e2360fcff39c3c20dee3d24d2760afb
YaroslavaKlymenko <klymenko0105@gmail.com>
2023-04-26 12:46:53 +0200

    change js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -19,56 +19,57 @@ const options = {
     onClose(selectedDates) {
         const selectedDate = selectedDates[0];
         const currentDate = new Date();
-        
 
         if (selectedDate < currentDate) {
-        Notiflix.Notify.failure("Please choose a date in the future");
-        startBtn.disabled = false;
+          Notiflix.Notify.failure("Please choose a date in the future");
         } else {
-        startBtn.disabled = true;
-        const timeDifference = selectedDate.getTime() - currentDate.getTime();
-        startCountdown(timeDifference);
-        dateTimePicker.disabled = true;
-  }
-        
+          const timeDifference = selectedDate.getTime() - currentDate.getTime();
+          startCountdown(timeDifference);
+          dateTimePicker.disabled = true;
+        }
     },
-  };
+};
 
-  flatpickr(dateTimePicker, options);
+flatpickr(dateTimePicker, options);
 
-  function startCountdown(timeDifference) {
-    clearInterval(countdownInterval);
-  
-    countdownInterval = setInterval(() => {
-      timeDifference -= 1000;
-  
-      const remainingDays = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-      const remainingHours = Math.floor(
-        (timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-      );
-      const remainingMinutes = Math.floor(
-        (timeDifference % (1000 * 60 * 60)) / (1000 * 60)
-      );
-      const remainingSeconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
-  
-      days.textContent = remainingDays.toString().padStart(2, "0");
-      hours.textContent = remainingHours.toString().padStart(2, "0");
-      minutes.textContent = remainingMinutes.toString().padStart(2, "0");
-      seconds.textContent = remainingSeconds.toString().padStart(2, "0");
-  
-      if (timeDifference <= 0) {
-        clearInterval(countdownInterval);
-        startBtn.disabled = false;
-        dateTimePicker.disabled = false;
+function startCountdown(timeDifference) {
+  clearInterval(countdownInterval);
+
+  countdownInterval = setInterval(() => {
+    timeDifference -= 1000;
+
+    if (timeDifference <= 0) {
+      clearInterval(countdownInterval);
+      startBtn.disabled = false;
+      dateTimePicker.disabled = false;
+      days.textContent = "00";
+      hours.textContent = "00";
+      minutes.textContent = "00";
+      seconds.textContent = "00";
+      return;
     }
-    }, 1000);
+
+    const remainingDays = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+    const remainingHours = Math.floor(
+      (timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    const remainingMinutes = Math.floor(
+      (timeDifference % (1000 * 60 * 60)) / (1000 * 60)
+    );
+    const remainingSeconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+    days.textContent = remainingDays.toString().padStart(2, "0");
+    hours.textContent = remainingHours.toString().padStart(2, "0");
+    minutes.textContent = remainingMinutes.toString().padStart(2, "0");
+    seconds.textContent = remainingSeconds.toString().padStart(2, "0");
+  }, 1000);
+}
+
+startBtn.addEventListener("click", () => {
+  startBtn.disabled = true;
+  const selectedDate = dateTimePicker.valueAsDate;
+  if (selectedDate) {
+    const timeDifference = selectedDate.getTime() - new Date().getTime();
+    startCountdown(timeDifference);
   }
-  
-  startBtn.addEventListener("click", () => {
-    const selectedDate = dateTimePicker.valueAsDate;
-    if (selectedDate) {
-      startCountdown(selectedDate.getTime() - new Date().getTime());
-      startBtn.disabled = true;
-    }
-  });
-
+});
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -13,12 +13,22 @@ function createPromise(position, delay) {
     })
 }
 
+
 const form = document.querySelector('.form')
-form.addEventListener('submit', (Event) => {
-  Event.preventDefault();
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  
+
   const delay = parseInt(form.elements.delay.value);
   const step = parseInt(form.elements.step.value);
-  const amount = parseInt(form.elements.amount.value)
+  const amount = parseInt(form.elements.amount.value);
+
+
+  if (!delay || delay < 1 || !step || step < 1 || !amount || amount < 1) {
+    Notiflix.Notify.failure("Please enter valid values for delay, step, and amount");
+    return;
+  }
+
 
   for (let i = 1; i <= amount; i ++) {
     const position = i;
